d1534fc1f29474aa94c02c0288da9d2e3cfb0913
FirozHossain82 <firozhossain295@gmail.com>
2023-07-29 11:17:48 +0600

    final commit shopping_js_concepts Projects

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-const cart = []; 
+// const cart = [];
 
-function displayProduct()
+/* function displayProduct()
 {
     let totalPrice = 0;
     const totalProducts = document.getElementById("total-products");
     totalProducts.innerText = cart.length;
 
-    const cartContainer = document.getElementById("products-cart");
+    const cartContainer = document.getElementById("cart-products");
     cartContainer.textContent = '';
 
     for(let i = 0; i < cart.length; i++)
@@ -31,15 +31,50 @@ function displayProduct()
 
     cartContainer.appendChild(tr);
 }
+ */
+const cartArray = [];
 
-function addToCart(element){
-   const pdName = element.parentNode.parentNode.children[0].innerText;
-    const pdPrice = element.parentNode.parentNode.children[1].children[0].innerText;
-    const pd = {
-        pdName :pdName,
-        pdPrice: parseInt (pdPrice)
-    }
-    cart.push(pd);
-    // console.log(cart)
-    displayProduct();
+function display(cartProduct) {
+  // console.log(cartProduct);
+ let totalPrice = 0;
+  const tableBody = document.getElementById("cart-products");
+  tableBody.innerHTML = " ";
+
+  for (let i = 0; i < cartProduct.length; i++) {
+    const name = cartArray[i].productName;
+    const price = cartArray[i].productPrice;
+
+    totalPrice = totalPrice + price;
+
+
+    const tr = document.createElement("tr");
+    // ${name}
+    tr.innerHTML = `
+       <th>${i + 1}</th>
+       <td>${name}</td>
+       <td>${price}</td>
+        `;
+    tableBody.appendChild(tr);
+  }
+  const tr = document.createElement("tr");
+  tr.innerHTML = `
+    <th></th>
+    <td>Total Price</td>
+    <td>${totalPrice}</td>
+    `;
+  tableBody.appendChild(tr);
+}
+
+function addToCart(element) {
+  const productName = element.parentNode.parentNode.children[0].innerText;
+  const productPrice =
+    element.parentNode.parentNode.children[1].children[0].innerText;
+  const productObj = {
+    productName: productName,
+    productPrice: parseInt(productPrice),
+  };
+  cartArray.push(productObj);
+  document.getElementById("total-products").innerText = cartArray.length;
+  // console.log(cart)
+  display(cartArray);
 }
